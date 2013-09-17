3c6ed89bac1879c074c1c1388995d0717ce1923e
dericpr <dericpr@gmail.com>
2013-09-16 23:16:28 -0400

    Improve the heuristic for handling cooling to prevent quick starts / stops.
    Increase timer for graphing from 1 second to 60 second intervals

--- scripts/crankbrew.lua
+++ scripts/crankbrew.lua
@@ -4,26 +4,17 @@ local device_path = '/sys/bus/w1/devices/28-00000449da30/w1_slave'
 local compressor_off_time = gre.mstime()
 local ssr_fired = false
 local report_temp_c = ""
-local points = ""
 local temp_c = 0
 local cur_x = 0;
 local points = {}
+local max_idx = 560 --[280
+local cooling_start = 0
+local cooling_end = 0
+
 configureOutGPIO(60)
 writeGPIO(60,0)
 
 
-local startx = 0
-local starty = 150
-local amplitude = 60
-local freq = 30
-local incx = 0
-local incy = 0
-local max_idx = 560 --[280
-local cur_w_idx = 1
-local wrapped = 0 
-local cur_rad = 0
-local x = {}
-local y = {}
 
 function read_temp()
 	local f = assert(io.open(device_path, 'r'))
@@ -43,8 +34,7 @@ function read_temp()
 	local get_data_table  = {}
   	get_data_table = gre.get_data("SetPoint")
   	local setPoint = get_data_table["SetPoint"] * 1 
-	print (string.format("Got temp of %s C and setpoint of %s C",temp_c, setPoint))
-	if ( temp_c > setPoint ) then
+	if ( temp_c > setPoint+0.5 ) then
 		if ( ssr_fired == false ) then
 			local data_table  = {}
 			local current_time = gre.mstime()
@@ -53,6 +43,7 @@ function read_temp()
 			if (  current_delay >= COMPRESSOR_DELAY ) then 
 				print("Turning on")
 				ssr_fired = true
+				cooling_start = gre.mstime()
 				data_table["Layer1.Cooling_On.grd_hidden"] = 0
 				data_table["Layer1.Compressor_Delay.grd_hidden"] = 1 
 				gre.set_data(data_table)
@@ -64,17 +55,19 @@ function read_temp()
 				gre.set_data(data_table)
 			end 
 		end	
-	else
+	else if ( temp_c < setPoint-0.2 ) then
 		if ( ssr_fired == true) then
 			local data_table  = {}
 			print("Turning off")
 			compressor_off_time = gre.mstime()
+			print(string.format("Compressor ran for %d seconds", (compressor_off_time - cooling_start)/1000 ))
 			ssr_fired = false
 			writeGPIO(60,0)
 			data_table["Layer1.Cooling_On.grd_hidden"] = 1
 			gre.set_data(data_table)
 		end
 	end
+end
 	
 end
 
@@ -97,9 +90,12 @@ function draw_trend(mapargs)
 	local v = {}
 	local newstr = ""
 		
+	-- Loop after 500 points
+	if ( cur_x > max_idx ) then
+		cur_x = 0
+	end
 	-- subtract max height of trend area to plot line from bottom left corner
 	newstr = string.format(" %d:%d",cur_x,200-temp_c )		
-	print(string.format("created new string %s", newstr))
 	table.insert(points, newstr)
 	cur_x = cur_x + 1
 	v["temps"] = table.concat(points, " ")
