309f6b45e1199fe530ce4e2a3fd0c228423a6b74
dericpr <dericpr@gmail.com>
2013-11-13 23:06:05 -0500

    Add second temp monitor

--- scripts/crankbrew.lua
+++ scripts/crankbrew.lua
@@ -1,12 +1,16 @@
 require ("gpio") -- import library
 local COMPRESSOR_DELAY = 180 -- 3 minute delay
 local device_path = '/sys/bus/w1/devices/28-00000449da30/w1_slave'
+local device_path_2 = '/sys/bus/w1/devices/28-000004f1d675/w1_slave'
 local compressor_off_time = gre.mstime()
 local ssr_fired = false
 local report_temp_c = ""
+local report_temp_2_c = ""
 local temp_c = 0
+local temp_2_c = 0
 local cur_x = 0;
 local points = {}
+local points2 = {}
 local max_idx = 560 --[280
 local cooling_start = 0
 local cooling_end = 0
@@ -17,20 +21,32 @@ writeGPIO(60,0)
 
 
 function read_temp()
+	
+	local temp
+	local temp_2
+	local data_table  = {}
 	local f = assert(io.open(device_path, 'r'))
 	local t = f:read("*all")
-	local temp
-	local data_table  = {}
-	
-	
 	f:close()
+	local second_device = assert(io.open(device_path_2, 'r'))
+	local t1 = second_device:read("*all")
+	second_device:close()
 	for w in string.gmatch(t,"t=*=(%d+)") do
 		temp = w
 	end
+	for m in string.gmatch(t1,"t=*=(%d+)") do
+		temp_2 = m
+	end
+	--print(string.format("Got temp 2 of %d",temp_2))
 	report_temp_c = string.format("%.2f",temp / 1000)
+	report_temp_2_c = string.format("%.2f", temp_2 / 1000)
+	--print(string.format("Temp2 in degrees c : %s",report_temp_2_c))
 	data_table["probeTemp"] = report_temp_c
+	data_table["probeTemp2"] = report_temp_2_c
 	gre.set_data(data_table)
+	
 	temp_c = temp/1000
+	temp_2_c = temp_2/1000
 	local get_data_table  = {}
   	get_data_table = gre.get_data("SetPoint")
   	local setPoint = get_data_table["SetPoint"] * 1 
@@ -49,7 +65,7 @@ function read_temp()
 				gre.set_data(data_table)
 				writeGPIO(60,1)
 			else
-				print(string.format("Current delay is %d seconds", current_delay))
+		--		print(string.format("Current delay is %d seconds", current_delay))
 				data_table["Layer1.Compressor_Delay.grd_hidden"] = 0
 				data_table["compDelay"] = string.format("%d",COMPRESSOR_DELAY-current_delay)
 				gre.set_data(data_table)
@@ -68,6 +84,7 @@ function read_temp()
 		end
 	end
 end
+
 	
 end
 
@@ -87,20 +104,24 @@ function show_control(mapargs)
 end
 
 function draw_trend(mapargs)
-	local v = {}
-	local newstr = ""
+--	local v = {}
+--	local newstr = ""
+--	local newstr2 = ""
 		
 	-- Loop after 500 points
-	if ( cur_x > max_idx ) then
-		cur_x = 0
-	end
+--	if ( cur_x > max_idx ) then
+--		cur_x = 0
+--	end
 	-- subtract max height of trend area to plot line from bottom left corner
-	newstr = string.format(" %d:%d",cur_x,200-temp_c )		
-	table.insert(points, newstr)
-	cur_x = cur_x + 1
-	v["temps"] = table.concat(points, " ")
+--	newstr = string.format(" %d:%d",cur_x,200-temp_c )	
+--	newstr2 = string.format(" %d:%d", cur_x,200-temp_2_c)	
+--	table.insert(points, newstr)
+--	table.insert(points2,newstr2)
+--	cur_x = cur_x + 1
+--	v["temps"] = table.concat(points, " ")
+--	v["temps2"] = table.concat(points2, " ")
 	
-	gre.set_data(v)
+--	gre.set_data(v)
 end
 
 function increment_setpoint(mapargs) 
